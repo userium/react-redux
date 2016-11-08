e375d63bc63ea5efd2fdc14138a02b08b15ef97f
Nina Patkai <nina.patkai@dsl-hkibrasgw4-50dc46-227.dhcp.inet.fi>
2016-11-08 14:16:44 +0200

    add components

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
 import reducers from './reducers';
-
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+import SearchBar from './components/search_bar'
+
+
+const App = () => {
+    return (
+    	<div>
+    		<SearchBar />
+    	</div>
+    );
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
